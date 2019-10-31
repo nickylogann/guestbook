d884b37280ec0e8aa2b8795c0e44b82f178fae24
nickylogann <laurentius.logan@tokopedia.com>
2019-10-31 12:37:28 +0700

    Add pagination to usecase

--- internal/endpoint/usecase/user/functions/handler.go
+++ internal/endpoint/usecase/user/functions/handler.go
@@ -2,18 +2,53 @@ package functions
 
 import (
 	"context"
+	"math"
 
 	"github.com/nickylogan/guestbook/internal/endpoint/models"
+	uModels "github.com/nickylogan/guestbook/internal/endpoint/usecase/user/models"
 )
 
 // FetchAll fetches all users with a pagination support
-func (u *userUseCase) FetchAll(ctx context.Context, name string, count, offset int) (users []models.User, err error) {
-	if count <= 0 {
-		count = 10
+// page uses 1-based indexing
+func (u *userUseCase) FetchAll(ctx context.Context, name string, page int) (response uModels.UserResponse, err error) {
+	// Init repo params
+	page = page - 1
+	if page < 0 {
+		page = 0
 	}
+	perPage := 20
+	offset := perPage * page
+
+	var users []models.User
+	var rowCount int
+
 	if name == "" {
-		return u.userRepo.Fetch(ctx, count, offset)
+		users, err = u.userRepo.Fetch(ctx, perPage, offset)
+		rowCount, err = u.userRepo.CountFetch(ctx, perPage, offset)
 	} else {
-		return u.userRepo.FetchFilterName(ctx, name, count, offset)
+		users, err = u.userRepo.FetchFilterName(ctx, name, perPage, offset)
+		rowCount, err = u.userRepo.CountFetchFilterName(ctx, name, perPage, offset)
 	}
+
+	numPages := int(math.Ceil(float64(rowCount) / float64(perPage)))
+
+	// Pagination logic
+	maxPage := 18
+	start := page - maxPage/2
+	if start < 1 {
+		start = 1
+	}
+	end := start + maxPage - 1
+	if end > numPages {
+		end = numPages
+	}
+
+	response = uModels.UserResponse{
+		Data:     users,
+		PrevPage: page > 1,
+		NextPage: page < (numPages - 1),
+		Start:    start,
+		End:      end,
+	}
+	return
 }
--- internal/endpoint/usecase/user/usecase.go
+++ internal/endpoint/usecase/user/usecase.go
@@ -3,10 +3,10 @@ package user
 import (
 	"context"
 
-	"github.com/nickylogan/guestbook/internal/endpoint/models"
+	"github.com/nickylogan/guestbook/internal/endpoint/usecase/user/models"
 )
 
 // UseCase represents use case related to the user
 type UseCase interface {
-	FetchAll(ctx context.Context, name string, count, offset int) (users []models.User, err error)
+	FetchAll(ctx context.Context, name string, page int) (res models.UserResponse, err error)
 }
