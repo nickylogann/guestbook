31f2fadf15ac001fa9dd5d11113c004b04400ad8
nickylogann <laurentius.logan@tokopedia.com>
2019-10-31 12:37:08 +0700

    Add count to repository

--- internal/endpoint/repository/user/postgres/get.go
+++ internal/endpoint/repository/user/postgres/get.go
@@ -2,27 +2,26 @@ package postgres
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/nickylogan/guestbook/internal/endpoint/models"
 )
 
 func (b *postgresRepository) Fetch(ctx context.Context, count, offset int) (users []models.User, err error) {
 	// Build query
-	q := fmt.Sprintf(`
+	q := `
 		SELECT
 			user_id, full_name, user_email, birth_date, create_time, update_time, 
 			date_part('year', age(birth_date)) as age
-		FROM
-			%s
+		FROM ` + b.tableName + `
 		WHERE
+			full_name IS NOT NULL AND
 			msisdn IS NOT NULL AND
 			birth_date IS NOT NULL
 		ORDER BY
 			full_name ASC
 		LIMIT $1
-		OFFSET $2
-	`, b.tableName)
+		OFFSET $2;
+	`
 
 	// Execute query
 	rows, err := b.db.Query(q, count, offset)
@@ -37,9 +36,37 @@ func (b *postgresRepository) Fetch(ctx context.Context, count, offset int) (user
 	users = make([]models.User, 0)
 	for rows.Next() {
 		var user models.User
-		rows.Scan(&user.ID, &user.Name, &user.Email, &user.BirthDate, &user.CreatedAt, &user.UpdatedAt, &user.Age)
+		err = rows.Scan(&user.ID, &user.Name, &user.Email, &user.BirthDate, &user.CreatedAt, &user.UpdatedAt, &user.Age)
 
 		users = append(users, user)
+	}
+	return
+}
+
+func (b *postgresRepository) CountFetch(ctx context.Context, count, offset int) (res int, err error) {
+	// Build query
+	q := `
+		SELECT
+			COUNT(user_id)
+		FROM ` + b.tableName + `
+		WHERE
+			full_name IS NOT NULL AND
+			msisdn IS NOT NULL AND
+			birth_date IS NOT NULL;
+	`
+
+	// Execute query
+	rows, err := b.db.Query(q)
+	if err != nil {
+		return
+	}
+	defer func() {
+		err = rows.Close()
+	}()
+
+	// Scan results
+	for rows.Next() {
+		err = rows.Scan(&res)
 	}
 	return
 }
@@ -58,7 +85,8 @@ func (b *postgresRepository) FetchFilterName(ctx context.Context, name string, c
 		ORDER BY
 			full_name ASC
 		LIMIT $2
-		OFFSET $3`
+		OFFSET $3;
+	`
 
 	// Execute query
 	filter := "%" + name + "%"
@@ -74,9 +102,38 @@ func (b *postgresRepository) FetchFilterName(ctx context.Context, name string, c
 	users = make([]models.User, 0)
 	for rows.Next() {
 		var user models.User
-		rows.Scan(&user.ID, &user.Name, &user.Email, &user.BirthDate, &user.CreatedAt, &user.UpdatedAt, &user.Age)
+		err = rows.Scan(&user.ID, &user.Name, &user.Email, &user.BirthDate, &user.CreatedAt, &user.UpdatedAt, &user.Age)
 
 		users = append(users, user)
+	}
+	return
+}
+
+func (b *postgresRepository) CountFetchFilterName(ctx context.Context, name string, count, offset int) (res int, err error) {
+	// Build query
+	q := `
+		SELECT
+			COUNT(user_id)
+		FROM ` + b.tableName + `
+		WHERE
+			msisdn IS NOT NULL AND
+			birth_date IS NOT NULL AND
+			full_name ILIKE $1;
+	`
+
+	// Execute query
+	filter := "%" + name + "%"
+	rows, err := b.db.Query(q, filter)
+	if err != nil {
+		return
+	}
+	defer func() {
+		err = rows.Close()
+	}()
+
+	// Scan results
+	for rows.Next() {
+		err = rows.Scan(&res)
 	}
 	return
 }
--- internal/endpoint/repository/user/repository.go
+++ internal/endpoint/repository/user/repository.go
@@ -8,5 +8,7 @@ import (
 
 type Repository interface {
 	Fetch(ctx context.Context, count, offset int) (users []models.User, err error)
+	CountFetch(ctx context.Context, count, offset int) (res int, err error)
 	FetchFilterName(ctx context.Context, name string, count, offset int) (users []models.User, err error)
+	CountFetchFilterName(ctx context.Context, name string, count, offset int) (res int, err error)
 }
