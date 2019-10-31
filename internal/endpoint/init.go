0ee57bd1f960f95b54c80d780583df126bd3c28b
nickylogann <laurentius.logan@tokopedia.com>
2019-10-31 13:59:19 +0700

    Add init

--- internal/endpoint/init.go
+++ internal/endpoint/init.go
@@ -1,18 +1,52 @@
 package endpoint
 
 import (
+	"fmt"
 	"log"
+	"os"
 
-	"github.com/nickylogan/guestbook/internal/pkg/config"
+	"github.com/labstack/echo"
+
+	"github.com/nickylogan/guestbook/internal/endpoint/delivery/http"
+	redisRepo "github.com/nickylogan/guestbook/internal/endpoint/repository/visitor/redis"
+	. "github.com/nickylogan/guestbook/internal/endpoint/usecase/user/functions"
+	. "github.com/nickylogan/guestbook/internal/endpoint/usecase/visitor/functions"
+	"github.com/nickylogan/guestbook/internal/pkg/connection/redis"
+
+	"github.com/joho/godotenv"
+
+	postgresUserRepo "github.com/nickylogan/guestbook/internal/endpoint/repository/user/postgres"
+	"github.com/nickylogan/guestbook/internal/pkg/connection/postgres"
+	"github.com/nickylogan/guestbook/internal/pkg/utils/config"
 )
 
 // Run runs the app
 func Run() {
-	cfg, err := config.NewConfig()
+	err := godotenv.Load("./configs/.env.development")
 	if err != nil {
-		log.Println("[error][endpoint][Run] failed to init config:", err)
+		log.Println(err)
 		return
 	}
-	// TODO: add init logic
-	_ = cfg
+	cfg := config.NewConfig()
+
+	dbConn, err := postgres.NewConnection(cfg.Database.Postgres)
+	if err != nil {
+		log.Fatal(err, cfg.Database.Postgres)
+	}
+	redisConn := redis.NewConnection(cfg.Database.Redis)
+
+	e := echo.New()
+	userRepo := postgresUserRepo.NewPostgresRepository(dbConn, &cfg, os.Getenv("PG_TABLE"))
+	userUseCase := NewUserUseCase(userRepo)
+
+	visitorRepo := redisRepo.NewRedisRepository(redisConn, os.Getenv("REDIS_KEY"))
+	visitorUseCase := NewVisitorUseCase(visitorRepo)
+
+	http.NewUserHandler(e, userUseCase, visitorUseCase)
+
+	fmt.Println("Server started at :8080")
+	err = e.Start(":8080")
+	if err != nil {
+		log.Fatal(err)
+	}
 }
